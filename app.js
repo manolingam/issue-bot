3cb9f3f9b10201636cae5c12ecb5ef7d9f5f10d9
Manolingam N <manolingam1996@gmail.com>
2020-07-21 00:03:48 +0530

    added root get route

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Welcome to Fork the World!");
+});
+
 app.post("/payload", (req, res) => {
     if (req.body.action === "labeled") {
         let issue = req.body.issue.html_url;
